d5222ebdf95d0f328b56c4e89aa13f50aab0b3e7
Joe Andaverde <joe.andaverde@gmail.com>
2016-01-04 23:09:37 -0600

    Use different gulp mocha plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var notifierReporter = require('mocha-notifier-reporter');
+var mocha = require('gulp-spawn-mocha');
 
 function test() {
-   process.env.NODE_ENV = 'test'
-
    return gulp.src(['test/*.js'], { read: false })
    .pipe(mocha({
-      reporter: notifierReporter.decorate('spec')
+      R: 'spec',
+      env: {'NODE_ENV': 'test'}
    }));
 }
 

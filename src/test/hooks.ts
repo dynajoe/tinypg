39fce0d51f74010f4e673f9d5b0465d4e13a4f72
Joe Andaverde <joe.andaverde@smrxt.com>
2018-11-30 15:27:56 -0600

    Add async hooks test

--- src/test/hooks.ts
+++ src/test/hooks.ts
@@ -1,6 +1,7 @@
 import * as H from './helper'
 import { expect } from 'chai'
 import { TinyPg } from '../'
+import * as AsyncHooks from 'async_hooks'
 
 describe.only('Hooks', () => {
    let tiny: TinyPg
@@ -44,6 +45,40 @@ describe.only('Hooks', () => {
             expect(final_context.onSubmit).to.not.be.null
             expect(final_context.onResult).to.not.be.null
          })
+      })
+   })
+
+   describe('with async_hooks', () => {
+      let final_context = null
+      let hooked_tiny = null
+
+      beforeEach(() => {
+         hooked_tiny = tiny.withHooks({
+            preSql: (ctx, name, params) => {
+               return {
+                  ctx: { ...ctx, preSql: { async_id: AsyncHooks.executionAsyncId(), trigger_id: AsyncHooks.triggerAsyncId() } },
+                  args: [name, params],
+               }
+            },
+            onQuery(ctx) {
+               return { ...ctx, onQuery: { async_id: AsyncHooks.executionAsyncId(), trigger_id: AsyncHooks.triggerAsyncId() } }
+            },
+            onSubmit(ctx) {
+               return { ...ctx, onSubmit: { async_id: AsyncHooks.executionAsyncId(), trigger_id: AsyncHooks.triggerAsyncId() } }
+            },
+            onResult(ctx) {
+               final_context = { ...ctx, onResult: { async_id: AsyncHooks.executionAsyncId(), trigger_id: AsyncHooks.triggerAsyncId() } }
+               return final_context
+            },
+         })
+      })
+
+      it('should run preSql in the same execution context', async () => {
+         const caller_async_execution_id = AsyncHooks.executionAsyncId()
+
+         await hooked_tiny.sql('a.select')
+
+         expect(final_context.preSql.async_id).to.equal(caller_async_execution_id)
       })
    })
 })
